5a41939fa3f700e7abe3fff9c387417226681508
Lais B <laisbelucom@gmail.com>
2022-08-12 03:29:49 -0300

    Favoritando os inputs dos certificados

--- js/certificados.js
+++ js/certificados.js
@@ -3,15 +3,33 @@ var arrayUl = [];
 
 botaoMore.addEventListener("click", function (event) {
     event.preventDefault();
+    
+    var li = document.createElement("li");
 
+    var ul = document.getElementById("ul");
+    ul.appendChild(li);
+    console.log(ul); 
+
+    var containerInput = document.createElement("div");
+    containerInput.classList.add("button__favorito");
     var inputCertificates = document.getElementById("cerficates");
     var inputCertificatesValue = inputCertificates.value;
-
+    
     var itemCertificados = document.createElement("input");
+    containerInput.appendChild(itemCertificados);
+    itemCertificados.classList.add("input-favorito");
+
+
+    var buttonFav = document.createElement("button");
+    containerInput.appendChild(buttonFav);
+    buttonFav.classList.add("button-favorito");
+
+    li.appendChild(containerInput);
+
 
     itemCertificados.value = inputCertificatesValue;
 
-    arrayUl.push(itemCertificados);
+    arrayUl.push(containerInput);
     console.log(arrayUl);
 
     if(arrayUl.length >= 4) { 
@@ -19,10 +37,4 @@ botaoMore.addEventListener("click", function (event) {
     }
 
     // criando li e add na ul
-    var li = document.createElement("li");
-    li.appendChild(itemCertificados);  
-
-    var ul = document.getElementById("ul");
-    ul.appendChild(li);
-    console.log(ul); 
 }); 
